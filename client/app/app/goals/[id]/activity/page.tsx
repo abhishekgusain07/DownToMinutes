a4a8d2f7b9fbde2b7ceb0279d356a94ac05cb68e
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-02-14 23:42:09 +0530

    styling

--- client/app/app/goals/[id]/activity/page.tsx
+++ client/app/app/goals/[id]/activity/page.tsx
@@ -3,7 +3,7 @@ import ActivityForGoals from "./_components/acitivityForGoals";
 const ActivityPage = async({ params }: { params: Promise<{ id: string }> }) => {
     const goalId = (await (params)).id;
     return (
-        <div>
+        <div className="w-full h-screen flex flex-col gap-2 items-center justify-center">
             <h1>Activity for Goal {goalId}</h1>
             <ActivityForGoals goalId={goalId}/>
         </div>
